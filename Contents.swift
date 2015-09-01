c5ebe9d2327f29ba94b1eb4315e4d18a787360bd
jaweber64 <jaweber64@gmail.com>
2015-09-01 17:56:17 -0500

    Constants_And_VariablesJanetW
    Week3 First assignment

---
+++ Contents.swift
@@ -0,0 +1,27 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Create 3 variables (declared, but not initialized)
+var age : Int            // the variable age is of the Int type
+var percentage : Float   // the variable percentage is of the Float type
+var rate : Double        // the variable rate is of the Double type
+
+// Create 3 constants (type is inferred)
+let year = 2012                // Int constant => year
+let currentPercentage = 5.9    // Float constant => currentPercentage
+let currentRate = 99.80655      // Double constant => currentRate
+
+// Create 3 explicit constants
+let firstName : String = "Janet"    // explicit String consant => firstName
+let percentage_2015 : Float = 5.9   // explicit Float constant => percentage_2015
+let acceleration : Double = 9.80655 // explicit Double constant => acceleration
+
+// Create and initialize a constant then change it's value on the  next line
+let test = 10
+// test = 12        Error is generated if this statement is not commented out.
+
+// Create a constant with explicit type of Float and a value of 4.
+let four : Float = 4    // The playground shows the value as 4, but it is stored                 
+                        // as 4.0 (a floating point number)
+
